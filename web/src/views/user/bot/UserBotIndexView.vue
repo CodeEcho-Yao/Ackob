063f62f15d345a18853d90dad3077f6d392c8c88
CodeEcho-Yao <1365560286@qq.com>
2023-02-21 15:44:23 +0800

    实现了导航栏 + 对战页面的地图和障碍物

---
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -0,0 +1,15 @@
+<template>
+  <ContentFiled> bot </ContentFiled>
+</template>
+
+<script>
+import ContentFiled from "@/components/ContentFiled.vue";
+
+export default {
+  components: {
+    ContentFiled,
+  },
+};
+</script>
+
+<style scoped></style>
